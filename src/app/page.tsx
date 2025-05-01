134fef34426ddac16ad7de3c496496d8d43e8f6a
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2025-05-01 06:51:12 +0200

    Adding more components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { Navbar } from "@/components/navbar";
+import CTA from "@/components/sections/cta/default";
+import FooterSection from "@/components/sections/footer/default";
 import Hero from "@/components/sections/hero/default";
+import Pricing from "@/components/sections/pricing/default";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,6 +15,9 @@ export default async function ProfileForm() {
     <main className="h-screen">
       <Navbar />
       <Hero />
+      <Pricing />
+      <CTA />
+      <FooterSection />
     </main>
   );
 }
